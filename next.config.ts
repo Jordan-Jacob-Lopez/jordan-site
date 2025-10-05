91cbd3c8e7f7b5c6dc890245dd2c06db473d5c97
Jordan Lopez <jordanlopez@Jordans-Air.attlocal.net>
2025-10-05 14:18:54 -0500

    adjusted lint file slightly

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
-  // leave TS strict; only flip the next line if you are truly blocked by TS:
-  // typescript: { ignoreBuildErrors: true },
 };
-
 export default nextConfig;
